31fd7a2ada5e3e4d8eaedd41ce6ee87a4018f183
hjkim88 <firadazer@gmail.com>
2020-06-22 16:51:22 -0500

    In implementation.

--- codes/Combine_GEX_And_TCR.R
+++ codes/Combine_GEX_And_TCR.R
@@ -21,8 +21,8 @@
 #                                 outputDir="./results/")
 ###
 
-combine_gex_tcr <- function(Seurat_RObj_path="./data/Ali_Tcell_agg.rds",
-                            TCR_data_path="./data/JCC280_VDJoutputs/",
+combine_gex_tcr <- function(Seurat_RObj_path="./data/JCC243_JCC280_Aggregregress.Robj",
+                            TCR_data_dirs="./data/JCC280_VDJoutputs/",
                             outputDir="./data/") {
   
   ### load library
@@ -30,6 +30,58 @@ combine_gex_tcr <- function(Seurat_RObj_path="./data/Ali_Tcell_agg.rds",
     install.packages("Seurat")
     require(Seurat, quietly = TRUE)
   }
+  
+  ### load the Seurat object and save the object name
+  tmp_env <- new.env()
+  load(Seurat_RObj_path, tmp_env)
+  obj_name <- ls(tmp_env)
+  assign("Seurat_Obj", get(obj_name, envir = tmp_env))
+  rm(tmp_env)
+  gc()
+  
+  ### rownames in the meta.data should be in the same order as colnames in the counts
+  Seurat_Obj@meta.data <- Seurat_Obj@meta.data[colnames(Seurat_Obj@assays$RNA@counts),]
+  
+  ### get TCR info file paths
+  TCR_data_dirs <- list.files(path = TCR_data_dirs, pattern = "filtered_contig_annotations.csv$",
+                              full.names = TRUE, recursive = TRUE)
+  
+  ### get Library and libNum info from the TCR files
+  tcr_libr_lib <- data.frame(matrix("", length(TCR_data_dirs), 2),
+                             stringsAsFactors = FALSE, check.names = FALSE)
+  rownames(tcr_libr_lib) <- basename(TCR_data_dirs)
+  colnames(tcr_libr_lib) <- c("Library", "LibNum")
+  for(i in 1:length(TCR_data_dirs)) {
+    extracted <- basename(TCR_data_dirs[i])
+    extracted <- substr(extracted, 15, nchar(extracted)-36)
+    extracted <- strsplit(extracted, split = "-lib", fixed = TRUE)[[1]]
+    
+    ### "B_cell" to "Bcell"
+    ### "PBMC_2" TO "PBMC-2"
+    if(grepl("B_cell", extracted[1])) {
+      extracted[1] <- paste0(strsplit(extracted[1], split = "B_cell", fixed = TRUE)[[1]][1], "Bcell")
+    } else if(grepl("PBMC_2", extracted[1])) {
+      extracted[1] <- paste0(strsplit(extracted[1], split = "PBMC_2", fixed = TRUE)[[1]][1], "PBMC-2")
+    }
+    
+    tcr_libr_lib[i,"Library"] <- extracted[1]
+    tcr_libr_lib[i,"LibNum"] <- extracted[2]
+  }
+  
+  ### check every TCR info has matched Library & LibNum in the Seurat object
+  for(i in 1:nrow(tcr_libr_lib)) {
+    if(length(which(Seurat_Obj@meta.data$Library == tcr_libr_lib[i,"Library"])) == 0) {
+      writeLines(paste(i, ":", tcr_libr_lib[i,"Library"]))
+    }
+  }
+  
+  
+  
+  
+  
+  
+  
+  
   
   ### load the Seurat object and save the object name
   Seurat_Obj <- readRDS(Seurat_RObj_path)
