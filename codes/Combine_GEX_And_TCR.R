00847a9323347a879a045b1ae4ec6b75ef76eae0
hjkim88 <firadazer@gmail.com>
2020-06-24 23:25:55 -0500

    In implementation.

--- codes/Combine_GEX_And_TCR.R
+++ codes/Combine_GEX_And_TCR.R
@@ -47,33 +47,113 @@ combine_gex_tcr <- function(Seurat_RObj_path="./data/JCC243_JCC280_Aggregregress
                               full.names = TRUE, recursive = TRUE)
   
   ### get Library and libNum info from the TCR files
-  tcr_libr_lib <- data.frame(matrix("", length(TCR_data_dirs), 2),
-                             stringsAsFactors = FALSE, check.names = FALSE)
-  rownames(tcr_libr_lib) <- basename(TCR_data_dirs)
-  colnames(tcr_libr_lib) <- c("Library", "LibNum")
+  tcr_libs <- rep("", length(TCR_data_dirs))
+  names(tcr_libs) <- basename(TCR_data_dirs)
   for(i in 1:length(TCR_data_dirs)) {
     extracted <- basename(TCR_data_dirs[i])
-    extracted <- substr(extracted, 15, nchar(extracted)-36)
-    extracted <- strsplit(extracted, split = "-lib", fixed = TRUE)[[1]]
+    extracted <- substr(extracted, 15, nchar(extracted)-41)
     
     ### "B_cell" to "Bcell"
     ### "PBMC_2" TO "PBMC-2"
-    if(grepl("B_cell", extracted[1])) {
-      extracted[1] <- paste0(strsplit(extracted[1], split = "B_cell", fixed = TRUE)[[1]][1], "Bcell")
-    } else if(grepl("PBMC_2", extracted[1])) {
-      extracted[1] <- paste0(strsplit(extracted[1], split = "PBMC_2", fixed = TRUE)[[1]][1], "PBMC-2")
+    if(grepl("B_cell", extracted)) {
+      extracted <- paste0(strsplit(extracted, split = "B_cell", fixed = TRUE)[[1]][1], "Bcell")
+    } else if(grepl("PBMC_2", extracted)) {
+      extracted <- paste0(strsplit(extracted, split = "PBMC_2", fixed = TRUE)[[1]][1], "PBMC-2")
     }
     
-    tcr_libr_lib[i,"Library"] <- extracted[1]
-    tcr_libr_lib[i,"LibNum"] <- extracted[2]
+    tcr_libs[i] <- extracted
   }
   
-  ### check every TCR info has matched Library & LibNum in the Seurat object
-  for(i in 1:nrow(tcr_libr_lib)) {
-    if(length(which(Seurat_Obj@meta.data$Library == tcr_libr_lib[i,"Library"])) == 0) {
-      writeLines(paste(i, ":", tcr_libr_lib[i,"Library"]))
+  ### check if every TCR info has matched Library in the Seurat object
+  keep_idx <- NULL
+  for(i in 1:length(tcr_libs)) {
+    if(length(which(Seurat_Obj@meta.data$Library == tcr_libs[i])) == 0) {
+      writeLines(paste(i, ":", tcr_libs[i]))
+    } else {
+      keep_idx <- c(keep_idx, i)
     }
   }
+  
+  ### retain the TCR files that are already in the Seurat object 
+  TCR_data_dirs <- TCR_data_dirs[keep_idx]
+  
+  ### remove BCR files - only include TCR files
+  TCR_data_dirs <- TCR_data_dirs[grep("TCR", TCR_data_dirs)]
+  
+  ### combine the TCR info
+  tcr <- NULL
+  for(i in 1:length(TCR_data_dirs)) {
+    
+    ### write progress
+    writeLines(paste(TCR_data_dirs[i]))
+    
+    ### load filtered contig annotation data
+    tcr_data <- read.csv(file = TCR_data_dirs[i],
+                         stringsAsFactors = FALSE, check.names = FALSE)
+    
+    ### remove the -* at the end of each barcode.
+    tcr_data$barcode <- sapply(strsplit(tcr_data$barcode, split = "-", fixed = TRUE), function(x) x[1])
+    
+    ### remove the rows that do not have CDR3 sequence
+    tcr_data <- tcr_data[which(tcr_data$cdr3 != "None"),]
+    
+    ### remove redundant rows
+    tcr_data <- tcr_data[!duplicated(tcr_data[c("barcode", "cdr3")]),]
+    
+    ### order by "chain" so that TRA rows come first than TRB rows
+    ### and secondly, order by "CDR3 Nucleotide" sequence
+    tcr_data <- tcr_data[order(as.character(tcr_data$cdr3_nt)),]
+    
+    ### merge different TRA & TRB info to one row
+    dups <- which(duplicated(tcr_data$barcode))
+    if(length(dups) > 0) {
+      temp <- tcr_data[dups,]
+      tcr_data <- tcr_data[-dups,]
+      rownames(tcr_data) <- tcr_data$barcode
+      for(barcode in tcr_data$barcode) {
+        idx <- which(temp$barcode == barcode)
+        tcr_data[barcode,"cdr3"] <- paste(c(tcr_data[barcode,"cdr3"], temp[idx, "cdr3"]), collapse = ";")
+        tcr_data[barcode,"cdr3_nt"] <- paste(c(tcr_data[barcode,"cdr3_nt"], temp[idx, "cdr3_nt"]), collapse = ";")
+        tcr_data[barcode,"productive"] <- paste(c(tcr_data[barcode,"productive"], temp[idx, "productive"]), collapse = ";")
+        tcr_data[barcode,"reads"] <- paste(c(tcr_data[barcode,"reads"], temp[idx, "reads"]), collapse = ";")
+        tcr_data[barcode,"umis"] <- paste(c(tcr_data[barcode,"umis"], temp[idx, "umis"]), collapse = ";")
+      }
+    }
+    
+    ### only retain informative columns
+    tcr_data <- tcr_data[,c("barcode", "raw_clonotype_id", "cdr3", "cdr3_nt", "reads", "umis", "productive")]
+    colnames(tcr_data) <- c("barcode", "raw_clonotype_id", "cdr3_aa", "cdr3_nt", "tcr_reads", "tcr_umis", "tcr_productive")
+    
+    ### add library info
+    tcr_data$library <- tcr_libs[basename(TCR_data_dirs[i])]
+    
+    ### combine the TCR data for all the data
+    if(is.null(tcr)) {
+      tcr <- tcr_data
+    } else {
+      tcr <- rbind(tcr, tcr_data)
+    }
+    
+  }
+  
+  ### attach the TCR info to the metadata of the Seurat object
+  obj_colnames <- colnames(Seurat_Obj@meta.data)
+  tcr_colnames <- colnames(tcr)
+  Seurat_Obj@meta.data$GexCellFull <- rownames(Seurat_Obj@meta.data)
+  Seurat_Obj@meta.data$GexCellShort <-  sapply(strsplit(rownames(Seurat_Obj@meta.data), split = "-", fixed = TRUE), function(x) x[1])
+  Seurat_Obj@meta.data <- merge(Seurat_Obj@meta.data,
+                                tcr,
+                                by.x = c("GexCellShort", "Library"),
+                                by.y = c("barcode", "library"),
+                                all.x = TRUE)
+  Seurat_Obj@meta.data <- Seurat_Obj@meta.data[,c("GexCellFull", "GexCellShort", obj_colnames, setdiff(tcr_colnames, c("barcode", "library")))]
+  rownames(Seurat_Obj@meta.data) <- Seurat_Obj@meta.data[,"GexCellFull"]
+  
+  
+  
+  
+  
+  
   
   
   
