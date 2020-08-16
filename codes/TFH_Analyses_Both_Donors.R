2956581e42a8a2aa618df1b5646a4815685fce30
hjkim88 <firadazer@gmail.com>
2020-08-16 16:44:45 -0500

    Skeleton2

--- codes/TFH_Analyses_Both_Donors.R
+++ codes/TFH_Analyses_Both_Donors.R
@@ -3,19 +3,66 @@
 #   Author    : Hyunjin Kim
 #   Date      : Aug 16, 2020
 #   Email     : hyunjin.kim@stjude.org
-#   Purpose   : 
+#   Purpose   : Now we have a new dataset from the other donor.
+#               Perform the same analyses and find shared clones between the two donors.
 #   
 #   Instruction
 #               1. Source("TFH_Analyses_Both_Donors.R")
-#               2. Run the function "tfh_analyses_both_donors" - specify the input file path and the output directory
+#               2. Run the function "tfh_analyses_both_donors" - specify the input file paths and the output directory
 #               3. The results will be generated under the output directory
 #
 #   Example
 #               > source("The_directory_of_TFH_Analyses_Both_Donors.R/TFH_Analyses_Both_Donors.R")
-#               > tfh_analyses_both_donors()
+#               > tfh_analyses_both_donors(Seurat_RObj_path="Z:/ResearchHome/ResearchHomeDirs/thomagrp/common/Hyunjin/SS_Tfh_BothDonors/Ali_bothDonors_Tcells_v2.rds",
+#                                          outputDir="./results/")
 ###
 
-tfh_analyses_both_donors <- function() {
+tfh_analyses_both_donors <- function(Seurat_RObj_path="Z:/ResearchHome/ResearchHomeDirs/thomagrp/common/Hyunjin/SS_Tfh_BothDonors/Ali_bothDonors_Tcells_v2.rds",
+                                     outputDir="./results/") {
+  
+  ### load libraries
+  if(!require(Seurat, quietly = TRUE)) {
+    install.packages("Seurat")
+    require(Seurat, quietly = TRUE)
+  }
+  options(java.parameters = "-Xmx10240m")
+  if(!require(xlsx, quietly = TRUE)) {
+    install.packages("xlsx")
+    require(xlsx, quietly = TRUE)
+  }
+  if(!require(ggplot2, quietly = TRUE)) {
+    install.packages("ggplot2")
+    require(ggplot2, quietly = TRUE)
+  }
+  if(!require(gridExtra, quietly = TRUE)) {
+    install.packages("gridExtra")
+    require(gridExtra, quietly = TRUE)
+  }
+  if(!require(ggalluvial, quietly = TRUE)) {
+    install.packages("ggalluvial")
+    require(ggalluvial, quietly = TRUE)
+  }
+  if(!require(ggpubr, quietly = TRUE)) {
+    install.packages("ggpubr")
+    require(ggpubr, quietly = TRUE)
+  }
+  if(!require(viridis, quietly = TRUE)) {
+    install.packages("viridis")
+    require(viridis, quietly = TRUE)
+  }
+  if(!require(ggsci, quietly = TRUE)) {
+    install.packages("ggsci")
+    require(ggsci, quietly = TRUE)
+  }
+  if(!require(org.Hs.eg.db, quietly = TRUE)) {
+    if (!requireNamespace("BiocManager", quietly = TRUE))
+      install.packages("BiocManager")
+    BiocManager::install("org.Hs.eg.db")
+    require(org.Hs.eg.db, quietly = TRUE)
+  }
+  
+  ### load Stefan's t-cell object
+  stefan_tcell_seurat_obj <- readRDS(Seurat_RObj_path)
   
   
   
