c63cb4efcc62b50b32209f8916e78de5b27b3457
Kim <hkim8@stjude.org>
2020-08-16 16:27:59 -0500

    Skeleton of the new dataset analyses

---
+++ codes/TFH_Analyses_Both_Donors.R
@@ -0,0 +1,23 @@
+###
+#   File name : TFH_Analyses_Both_Donors.R
+#   Author    : Hyunjin Kim
+#   Date      : Aug 16, 2020
+#   Email     : hyunjin.kim@stjude.org
+#   Purpose   : 
+#   
+#   Instruction
+#               1. Source("TFH_Analyses_Both_Donors.R")
+#               2. Run the function "tfh_analyses_both_donors" - specify the input file path and the output directory
+#               3. The results will be generated under the output directory
+#
+#   Example
+#               > source("The_directory_of_TFH_Analyses_Both_Donors.R/TFH_Analyses_Both_Donors.R")
+#               > tfh_analyses_both_donors()
+###
+
+tfh_analyses_both_donors <- function() {
+  
+  
+  
+  
+}
