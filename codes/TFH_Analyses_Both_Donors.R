05790e905230974b9d45c25cf7030d0083fb0b07
hjkim88 <firadazer@gmail.com>
2020-10-13 22:28:16 -0500

    draw_lineage_network() in implementation.

--- codes/TFH_Analyses_Both_Donors.R
+++ codes/TFH_Analyses_Both_Donors.R
@@ -1926,6 +1926,24 @@ tfh_analyses_both_donors <- function(Seurat_RObj_path="./data/SS_Tfh_BothDonors/
       require(igraph, quietly = TRUE)
     }
     
+    ### first build an adjacency matrix
+    ### rows: outbound
+    ### columns: inbound
+    
+    adjm <- matrix(sample(0:1, 100, replace=TRUE, prob=c(0.9,0.1)), nc=10)
+    g1 <- graph_from_adjacency_matrix( adjm )
+    adjm <- matrix(sample(0:5, 100, replace=TRUE,
+                          prob=c(0.9,0.02,0.02,0.02,0.02,0.02)), nc=10)
+    g2 <- graph_from_adjacency_matrix(adjm, weighted=TRUE)
+    E(g2)$weight
+    
+    ## various modes for weighted graphs, with some tests
+    nzs <- function(x) sort(x [x!=0])
+    adjm <- matrix(runif(100), 10)
+    adjm[ adjm<0.5 ] <- 0
+    g3 <- graph_from_adjacency_matrix((adjm + t(adjm))/2, weighted=TRUE,
+                                      mode="undirected")
+    
     
     
   }
